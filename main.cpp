12a0b3639fc2abe276fc20895f0f500171f12c4d
Kristina Grishanina <kristyagrishanina@mail.ru>
2022-05-02 19:03:28 +0500

    com 10

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@ using namespace std;
 Ke 418
 Grishanina raschektaeva
 30 04 2022
+Ke 418
 */
 int main()
 {
 
     int a, b;
     cout << "Enter A and B: "; cin >> a >> b;
-    cin >> a-b;
+    cout << "A + B = " << a + b << '\n' << "A - B = " << a - b << '\n' << "A * B = " << a * b << '\n'; return 0;
     return 0;
 }
