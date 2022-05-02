f85c7e9b2085d5052fab92d285b9521ffcd16128
Kristina Grishanina <kristyagrishanina@mail.ru>
2022-05-02 19:05:27 +0500

    com 11

--- main.cpp
+++ main.cpp
@@ -12,6 +12,6 @@ int main()
 
     int a, b;
     cout << "Enter A and B: "; cin >> a >> b;
-    cin >> a-b;
+    cout << "A + B = " << a + b << '\n' << "A - B = " << a - b << '\n' << "A / B = " << a / b << '\n'; return 0;
     return 0;
 }
